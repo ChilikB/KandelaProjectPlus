71296668eb9b9392ba1dbfffa1ae057a53e324e4
ChilikB <bohdan.kandela@gmail.com>
2026-02-01 14:59:41 +0200

    Initialize

---
+++ Source/KandelaProjectPlus/KandelaProjectPlusGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KandelaProjectPlusGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKandelaProjectPlusGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKandelaProjectPlusGameMode();
+};
+
+
+
