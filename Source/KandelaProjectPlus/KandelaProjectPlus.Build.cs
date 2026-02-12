3126fe46332db83d7fc17e69c4d6601b576147ed
ChilikB <bohdan.kandela@gmail.com>
2026-02-12 16:05:54 +0200

    fix(): structure

--- Source/KandelaProjectPlus/KandelaProjectPlus.Build.cs
+++ Source/KandelaProjectPlus/KandelaProjectPlus.Build.cs
@@ -4,10 +4,30 @@ using UnrealBuildTool;
 
 public class KandelaProjectPlus : ModuleRules
 {
-	public KandelaProjectPlus(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public KandelaProjectPlus(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags" });
+        
+        PublicIncludePaths.AddRange(new[]
+        {
+            ModuleDirectory + "/Public",
+            ModuleDirectory + "/Public/GAS",
+            ModuleDirectory + "/Public/GAS/Abilities",
+            ModuleDirectory + "/Public/GAS/Abilities/Sprint",
+            ModuleDirectory + "/Public/GAS/Core",
+            ModuleDirectory + "/Public/GAS/Effects"
+        });
+
+        PrivateIncludePaths.AddRange(new[]
+        {
+            ModuleDirectory + "/Private",
+            ModuleDirectory + "/Private/GAS",
+            ModuleDirectory + "/Private/GAS/Abilities",
+            ModuleDirectory + "/Private/GAS/Abilities/Sprint",
+            ModuleDirectory + "/Private/GAS/Core",
+            ModuleDirectory + "/Private/GAS/Effects"
+        });
+    }
 }
